241e227505054f46501ddd75627cb9fa8edd09c3
aamirkhan237 <aamir.4u71@gmail.com>
2024-06-12 01:22:51 +0530

    api ph-1

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::TasksController < ApiController
+    before_action :set_task, only: [:show, :update, :destroy, :update_status]
+
+    def index
+        @tasks = current_user.tasks.filter_by_status(params[:status])
+        render json: @tasks
+      end
+    def show
+        render json: @task
+    end
+
+    def create
+        @task = current_user.tasks.build(task_params)
+        if @task.save
+          render json: @task, status: :created
+        else
+          render json: @task.errors, status: :unprocessable_entity
+        end
+      end
+
+
+      def update
+        if @task.update(task_params)
+            render json: @task
+          else
+            render json: @task.errors, status: :unprocessable_entity
+          end
+        end
+      def destroy
+        @task.destroy
+        head :no_content  # Respond with 204 No Content on successful deletion
+      end
+
+      def update_status
+        if @task.update(status: params[:status])
+          render json: @task
+        else
+          render json: @task.errors, status: :unprocessable_entity
+        end
+      end
+
+    private
+    def set_task
+        @task = current_user.tasks.find(params[:id])
+    end
+
+    def task_params
+        params.require(:task).permit(:title, :description, :status)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
       sessions: 'users/sessions'
     }
     resources :users
+
+    namespace :api do
+      namespace :v1 do
+        resources :tasks do
+          patch :update_status, on: :member
+        end
+        resources :users
+      end
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
